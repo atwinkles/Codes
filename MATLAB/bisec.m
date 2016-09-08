8f95884c9a65ba990360571ec02247e65ab74480
winky94 <atwinkles@mindspring.com>
2016-09-08 09:01:42 -0400

    newton, steffensen

--- MATLAB/bisec.m
+++ MATLAB/bisec.m
@@ -1,6 +1,5 @@
-function y = bisec(f,a,b,T,S)
-% y = bisect(f,a,b,D,E)
-%
+function bisec(f,a,b,T,S,q)
+% bisect(f,a,b,D,E)
 %This is a bisection algorithm by Alexander Winkles used to find roots of 
 % polynomials over a domain.
 %
@@ -9,19 +8,25 @@ function y = bisec(f,a,b,T,S)
 % b : the upper domain value
 % T : the tolerance of the final result
 % S : the number of iterations
+% q : prints the iterations (1 is yes, 0 is no)
 
 
 if sign(feval(f,a))==sign(feval(f,b))
-    disp('Error: f(a) and f(b) have the same signs.');
+    disp('\nError: f(a) and f(b) have the same signs.\n\n');
 else
+    if q == 1
+        fprintf('\nStep \t\t Result\n---\t\t --------\n');
+    end;
     i = 0;
     while i <= S
         e = b - a;
         e = e/2;
         c = a + e;
+        if q  == 1
+            fprintf('%d \t\t %f\n',i,c)
+        end;
         if feval(f,c) == 0 || abs(feval(f,c)) < T
-            y = c;
-            fprintf('\nThe solution is %d.\n The computation was a success after %d iterations!',y,i)
+            fprintf('\nThe solution is %d. The computation was a success after %d iterations!\n\n',c,i)
             break;
         end;
         i = i+1;
@@ -32,7 +37,7 @@ else
         end;
     end;
     if i == S+1
-        fprintf('Method failed after %d iterations.',S)
+        fprintf('\nMethod failed after %d iterations.\n\n',S)
     end;
 end;
     
